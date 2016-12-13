eb2d30b8f6de0f05694379077b0552d5d6a40248
mmatosoliveira <mmatos.oliver@gmail.com>
2016-12-12 23:45:41 -0200

    Add files via upload
    inserção manual da controller da consulta do acervo.

---
+++ src/br/com/casafabianodecristo/biblioteca/view/ConsultarAcervoController.java
@@ -0,0 +1,14 @@
+package br.com.casafabianodecristo.biblioteca.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class ConsultarAcervoController {
+	
+	@FXML
+	private Button botaoFechar;
+	
+	@FXML
+	private void initializae(){}
+
+}
